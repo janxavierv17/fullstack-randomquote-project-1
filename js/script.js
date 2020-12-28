a7630be0c52f1aa19f33f8e8026b1302980b0346
janxavierv <37735268+janxavierv@users.noreply.github.com>
2020-12-28 23:24:57 +1100

    going for exceeds expectations

--- js/script.js
+++ js/script.js
@@ -16,36 +16,51 @@ let quotes = [
     source:"Ernest Hemingway",
     citation:"The Quotation Page",
     year:"1899 - 1961",
+    tags:"Tag #1"
   },
   {
     quote: "Take the diplomacy out of war and the thing would fall flat in a week.",
     source:"Will Rogers",
     citation:"The Quotation Page",
     year:"1879 - 1935",
+    tags:"Tag #2"
   },
   {
     quote: "This is the challenge of writing. You have to be very emotionally engaged in what you’re doing, or it comes out flat. You can’t fake your way through this.",
     source:"Gordon Atkinson",
     citation:"The Quotation Page",
-    year:"",
+    year:"2004",
+    tags:"Tag #3"
   },
   {
     quote: "The only function of economic forecasting is to make astrology look respectable.",
     source:"John Kenneth Galbraith",
     citation:"The Quotation Page",
     year:"1908 - 2006",
+    tags:"Tag #4"
   },
   {
     quote: "Cherish each hour of this day for it can never return.",
     source:"Og Mandino",
     citation:"The Quotation Page",
     year:"1923 - 1996",
+    tags:"Tag #5"
   },
-  
 ]
+
+var colors = [
+  "#E0BBE4",
+  "#957DAD",
+  "#D291BC",
+  "#770928",
+  "#FEC8D8",
+  "#FFDFD3",
+];
 /***
  * `getRandomQuote` function
 ***/
+
+
 let getRandomQuote = () => {
   let randomNumber = Math.floor((Math.random() * quotes.length));
   return quotes[randomNumber];
@@ -56,7 +71,10 @@ let getRandomQuote = () => {
 let printQuote = () => {
   let quoteBox = document.getElementById("quote-box")
   let randomQuotes = getRandomQuote();
-
+  let randomNumber = Math.floor((Math.random() * quotes.length));
+  let randomCol = colors[randomNumber];
+  
+  document.body.style.background = randomCol;
   let tags = `
   <p class="quote">${randomQuotes.quote}</p>
   <p class="source">${randomQuotes.source}
@@ -67,6 +85,9 @@ let printQuote = () => {
   return html;
 }
 
+setInterval(()=>{
+  printQuote();
+}, 15000)
 
 /***
  * click event listener for the print quote button
